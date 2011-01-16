c68cdaa6b0757a39f957965555585f4150aac324
Farina <andref5@gmail.com>
2011-01-16 14:07:55 -0200

    move to jasmine for test js

---
+++ spec/CruyffSpec.js
@@ -0,0 +1,5 @@
+describe('Cruyff',function(){
+  it('ajax crud',function(){
+
+  })
+});
--- spec/unit/spec.js
+++
@@ -1,8 +0,0 @@
-
-describe 'Cruyff'
-  describe '.someMethod()'
-    it 'should do something'
-      true.should.be true
-    end
-  end
-end
