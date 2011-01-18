a56ef7cf02469f98bdb385a2031014dcd61051d9
Farina <andref5@gmail.com>
2011-01-18 00:02:35 -0200

    create test for bind click

--- spec/CruyffSpec.js
+++ spec/CruyffSpec.js
@@ -1,5 +1,7 @@
 describe('Cruyff',function(){
-  it('ajax crud',function(){
-
+  it('should render success ajax call',function(){
+    $.jasmine.inject('<a href="spec/fixtures/view" data-remote="true">remote_link</a>');
+    var remote_link = $('a[data-remote]');
+    expect(remote_link.data('events')).toContain('click');
   })
 });
