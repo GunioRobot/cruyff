7ddce2905b76fd6394db04ccbf42315aa6079c83
André Farina <afarina@mm-java-05.(none)>
2011-01-18 19:11:02 -0200

    load url

--- spec/CruyffSpec.js
+++ spec/CruyffSpec.js
@@ -1,8 +1,32 @@
 describe('Cruyff',function() {
+  var remote_link,
+      link_url,
+      remote_form,
+      form_url;
+
+  beforeEach(function () {
+    $.jasmine.inject('<a href="spec/fixtures/view.html" data-remote="true">remote_link</a>');
+    remote_link = $('a[data-remote]')[0];
+    link_url = $.fn.crudSettings($(remote_link));
+
+    $.jasmine.inject('<form action="spec/fixtures/view.html" data-remote="true"></form>');
+    remote_form = $('form[data-remote]')[0];
+    form_url = $.fn.crudSettings($(remote_form));
+  });
+
+
   it('should override rails handleRemote function',function(){
     spyOn($.fn, 'handleRemoteCrud');
     $.fn.handleRemote('element');
     expect($.fn.handleRemoteCrud).wasCalled();
+  })
+
+  it('should get url from form',function(){
+    expect(form_url).toEqual('spec/fixtures/view.html');
+  })
+
+  it('should get url from hiperlink',function(){
+    expect(link_url).toEqual('spec/fixtures/view.html');
   })
 
   xit('should call ajax success function for hyperlink with data-remote',function(){
