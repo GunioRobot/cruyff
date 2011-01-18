6ee8f0ec8b5945d193341a4133a7d5f01d34691d
André Farina <afarina@mm-java-05.(none)>
2011-01-18 17:11:03 -0200

    override rails jquery handleRemote

--- spec/CruyffSpec.js
+++ spec/CruyffSpec.js
@@ -1,7 +1,17 @@
-describe('Cruyff',function(){
-  it('should render success ajax call',function(){
-    $.jasmine.inject('<a href="spec/fixtures/view" data-remote="true">remote_link</a>');
-    var remote_link = $('a[data-remote]');
-    expect(remote_link.data('events')).toContain('click');
+describe('Cruyff',function() {
+  it('should override rails handleRemote function',function(){
+    spyOn($.fn, 'handleRemoteCrud');
+    $.fn.handleRemote('element');
+    expect($.fn.handleRemoteCrud).wasCalled();
+  })
+
+  xit('should call ajax success function for hyperlink with data-remote',function(){
+    $.jasmine.inject('<a href="spec/fixtures/view.html" data-remote="true">remote_link</a>');
+    var call_success;
+    var remote_link = $('a[data-remote]');    
+    remote_link.live('ajax:success', function(e, data, status, xhr) { 
+      call_success = true;
+    }).trigger('click');
+    expect(call_success).toBeTruthy();
   })
 });
