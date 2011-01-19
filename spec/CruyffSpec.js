8a64aab9f3b5ff36e7c425148e67eac9528f1b15
André Farina <afarina@mm-java-05.(none)>
2011-01-19 14:04:02 -0200

    load attr method

--- spec/CruyffSpec.js
+++ spec/CruyffSpec.js
@@ -1,17 +1,21 @@
 describe('Cruyff',function() {
   var remote_link,
       link_url,
+      link_method,
       remote_form,
-      form_url;
+      form_url,
+      form_method;
 
   beforeEach(function () {
     $.jasmine.inject('<a href="spec/fixtures/view.html" data-remote="true">remote_link</a>');
     remote_link = $('a[data-remote]')[0];
-    link_url = $.fn.crudSettings($(remote_link));
+    link_url = $.fn.crudSetup($(remote_link)).url;
+    link_method = $.fn.crudSetup($(remote_link)).method;
 
-    $.jasmine.inject('<form action="spec/fixtures/view.html" data-remote="true"></form>');
+    $.jasmine.inject('<form action="spec/fixtures/view.html" method="post" data-remote="true"></form>');
     remote_form = $('form[data-remote]')[0];
-    form_url = $.fn.crudSettings($(remote_form));
+    form_url = $.fn.crudSetup($(remote_form)).url;
+    form_method = $.fn.crudSetup($(remote_form)).method;
   });
 
 
@@ -25,9 +29,14 @@ describe('Cruyff',function() {
     expect(form_url).toEqual('spec/fixtures/view.html');
   })
 
+  it('should get method from form',function(){
+    expect(form_method).toEqual('post');
+  })
+
   it('should get url from hiperlink',function(){
     expect(link_url).toEqual('spec/fixtures/view.html');
   })
+
 
   xit('should call ajax success function for hyperlink with data-remote',function(){
     $.jasmine.inject('<a href="spec/fixtures/view.html" data-remote="true">remote_link</a>');
